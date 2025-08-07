1d3a02963eed37abd3d795c531b3a97148eca646
Oleksii Kovsh <kovsh1999@gmail.com>
2025-08-07 21:20:19 +0300

    add validation for order-modal

--- src/js/order-modal.js
+++ src/js/order-modal.js
@@ -1,3 +1,7 @@
+import validator from 'validator';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const backdrop = document.querySelector('[data-order-modal-backdrop]');
 const openModalBtn = document.querySelector('[data-order-modal-open]');
 const closeModalBtn = document.querySelector('[data-order-modal-close]');
@@ -9,13 +13,34 @@ btnOrder.disabled = true;
 if (openModalBtn) {
   openModalBtn.addEventListener('click', openModal);
 }
+
 form.addEventListener('submit', handleSubmit);
 document.addEventListener('keydown', handleEscape);
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('mousedown', closeModal);
+
+openModal();
 
 function openModal() {
   backdrop.classList.add('is-open');
   document.body.classList.add('no-scroll');
+
+  const requiredInputs = form.querySelectorAll('input[required]');
+
+  requiredInputs.forEach(input => {
+    input.addEventListener('input', checkInputs);
+  });
+
+  function checkInputs() {
+    let allValid = true;
+
+    requiredInputs.forEach(input => {
+      if (input.value.trim() === '') {
+        allValid = false;
+      }
+    });
+
+    btnOrder.disabled = !allValid;
+  }
 }
 
 function closeModal(event) {
@@ -38,7 +63,35 @@ function handleEscape(event) {
 
 function handleSubmit(event) {
   event.preventDefault();
-  console.log('Форма відправлена');
+
+  const email = form.email.value.trim();
+  const tel = form.tel.value.trim().replace(/\D/g, '');
+  const comment = form.text.value.trim();
+
+  if (!validator.isEmail(email)) {
+    iziToast.error({
+      title: 'Помилка',
+      message: `Невалідний email`,
+      position: 'topRight',
+    });
+    return;
+  }
+
+  if (!validator.isMobilePhone(tel, 'uk-UA')) {
+    iziToast.error({
+      title: 'Помилка',
+      message: `Невалідний номер телефону`,
+      position: 'topRight',
+    });
+    return;
+  }
+
+  console.log({
+    email,
+    tel,
+    comment,
+  });
+
   backdrop.classList.remove('is-open');
   form.reset();
 }
